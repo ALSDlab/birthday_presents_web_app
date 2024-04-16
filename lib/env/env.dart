c1d23753da75c6d9bf41ae8d9add421e236547e4
ssd72 <sungdae.yi@gmail.com>
2024-04-16 11:08:31 +0200

    feat: screenutil 패키지 적용, 폰트 추가, .env파일 추가

---
+++ lib/env/env.dart
@@ -0,0 +1,25 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: 'lib/env/.env', useConstantCase: true)
+abstract class Env {
+
+  // SAMPLE_ACCOUNT_INFO
+  @EnviedField()
+  static const String sampleAccountUserid = _Env.sampleAccountUserid;
+  @EnviedField()
+  static const String sampleAccountUsername = _Env.sampleAccountUsername;
+  @EnviedField()
+  static const String sampleAccountEmail = _Env.sampleAccountEmail;
+  @EnviedField()
+  static const String sampleAccountPassword = _Env.sampleAccountPassword;
+
+  // // ANDROID_APPLICATION_ID
+  // @EnviedField()
+  // static const String androidApplicationId = _Env.androidApplicationId;
+  //
+  // // IOS_APPLICATION_ID
+  // @EnviedField()
+  // static const String iosApplicationId = _Env.iosApplicationId;
+}
