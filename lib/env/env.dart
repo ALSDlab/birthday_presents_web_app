806c1326499b93dfa361106f46577bea24bf74c3
ssd72 <sungdae.yi@gmail.com>
2024-04-17 10:02:49 +0200

    feat: env파일 수정

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -6,20 +6,20 @@ part 'env.g.dart';
 abstract class Env {
 
   // SAMPLE_ACCOUNT_INFO
-  @EnviedField()
-  static const String sampleAccountUserid = _Env.sampleAccountUserid;
-  @EnviedField()
-  static const String sampleAccountUsername = _Env.sampleAccountUsername;
-  @EnviedField()
-  static const String sampleAccountEmail = _Env.sampleAccountEmail;
-  @EnviedField()
-  static const String sampleAccountPassword = _Env.sampleAccountPassword;
+  // @EnviedField()
+  // static const String sampleAccountUserid = _Env.sampleAccountUserid;
+  // @EnviedField()
+  // static const String sampleAccountUsername = _Env.sampleAccountUsername;
+  // @EnviedField()
+  // static const String sampleAccountEmail = _Env.sampleAccountEmail;
+  // @EnviedField()
+  // static const String sampleAccountPassword = _Env.sampleAccountPassword;
 
-  // // ANDROID_APPLICATION_ID
-  // @EnviedField()
-  // static const String androidApplicationId = _Env.androidApplicationId;
-  //
-  // // IOS_APPLICATION_ID
-  // @EnviedField()
-  // static const String iosApplicationId = _Env.iosApplicationId;
+  // ANDROID_APPLICATION_ID
+  @EnviedField()
+  static const String androidApplicationId = _Env.androidApplicationId;
+
+  // IOS_APPLICATION_ID
+  @EnviedField()
+  static const String iosApplicationId = _Env.iosApplicationId;
 }
