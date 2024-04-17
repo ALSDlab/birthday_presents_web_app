e96f359ff34fafe2422d9b35debc0b775d77eb70
ssd72 <sungdae.yi@gmail.com>
2024-04-17 10:02:49 +0200

    add: bottomNavigationBar 추가. feat: env파일, main.dart, router.dart파일 기본 구조 작성

---
+++ lib/router.dart
@@ -0,0 +1,11 @@
+import 'package:go_router/go_router.dart';
+import 'package:myk_market_app/view/page/main_page/navigation.dart';
+
+final router = GoRouter(initialLocation: '/main_page', routes: [
+  // 회사소개 메인페이지
+  GoRoute(
+      path: '/main_page',
+      builder: (context, state) => Navigation(selectedIndex: 0,
+      ),),
+
+]);
