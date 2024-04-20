647fede1b066aa3af6b41de7698dd9b97e191dbd
hugedreamhd <hugedreamhd@gmail.com>
2024-04-20 21:41:31 +0900

    feat: mainpage 기본 툴 작성

--- lib/view/page/main_page/main_page.dart
+++ lib/view/page/main_page/main_page.dart
@@ -1,22 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myk_market_app/view/page/main_page/store_view_model.dart';
+import 'package:provider/provider.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  void initState() {
+    Future.microtask(() {
+      final StoreViewModel viewModel = context.read<StoreViewModel>();
+      viewModel.getStoreList();
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<StoreViewModel>();
+    final state = viewModel.state;
     return Scaffold(
       appBar: AppBar(
-        title: Text('회사소개'),
+        title: Text('민영기 염소탕 회사소개'),
       ),
       body: Column(
         children: [
           Row(
             children: [
-              Image.network('회사 이미지1'),
-              Text('BRAND STORY'),
+              Expanded(child: Image.network(state.storeList[0].titleImage)),
             ],
-          )
+          ),
+          Text('BRAND STORY'),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(state.storeList[0].introText),
+          ),
         ],
       ),
     );
---
+++ lib/view/page/main_page/store_view_model.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:myk_market_app/data/repository/store_repository_impl.dart';
+import 'package:myk_market_app/view/page/main_page/store_state.dart';
+
+class StoreViewModel extends ChangeNotifier {
+  StoreRepositoryImpl repository = StoreRepositoryImpl();
+  StoreState _state = StoreState();
+
+  StoreState get state => _state;
+
+  void getStoreList() async {
+    _state = state.copyWith(storeList: await repository.getFirebaseStore());
+    notifyListeners();
+  }
+
+}
