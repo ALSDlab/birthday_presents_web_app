1af5f12fed5792dd8821add89fcd45e772f2613f
ssd72 <sungdae.yi@gmail.com>
2024-04-22 09:09:16 +0200

    feat: 주소검색 기능 앱,웹 연동 완료(에러메세지 포함)

--- lib/view/page/signup_page/platform_check/check_file.dart
+++ lib/view/page/signup_page/platform_check/check_file.dart
@@ -1,2 +1,3 @@
-export 'package:myk_market_app/view/page/signup_page/platform_check/check_pack/app.dart'
+export 'package:myk_market_app/view/page/signup_page/platform_check/check_pack/err.dart'
+if (dart.library.io) 'package:myk_market_app/view/page/signup_page/platform_check/check_pack/app.dart'
 if (dart.library.html) 'package:myk_market_app/view/page/signup_page/platform_check/check_pack/web.dart';
