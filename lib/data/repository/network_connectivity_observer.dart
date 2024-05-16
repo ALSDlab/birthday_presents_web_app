3f50ba5484d5dd7d1c24267b631e4d57d925d982
hugedreamhd <hugedreamhd@gmail.com>
2024-05-16 16:20:48 +0900

    feat: mainpage내 network 통신상태 연결확인 코드 작성중

--- lib/data/repository/network_connectivity_observer.dart
+++ lib/data/repository/network_connectivity_observer.dart
@@ -4,27 +4,36 @@ import 'package:myk_market_app/data/repository/connectivity_observer.dart';
 class NetworkConnectivityObserver implements ConnectivityObserver {
   final _connectivity = Connectivity();
 
+
   @override
   Stream<Status> observe() {
-    return _connectivity.onConnectivityChanged.where((event) {
+    return _connectivity.onConnectivityChanged.map((event) {
       print('Connetivity changed : $event');
-      return event == ConnectivityResult.ethernet ||
+      var connectivityResult = event.first; //첫번째 요소만 가져옴
+
+      return connectivityResult == ConnectivityResult.ethernet ||
           event == ConnectivityResult.mobile ||
           event == ConnectivityResult.wifi ||
-          event == ConnectivityResult.none;
-    }).map((event) {
-      //print('Connetivity changed2 : $event');
-      switch (event) {
-
-        case ConnectivityResult.wifi:
-        case ConnectivityResult.ethernet:
-        case ConnectivityResult.mobile:
-          return Status.available;
-
-        default:
-
-          return Status.unavailable;
-      }
+          event == ConnectivityResult.none
+          ? Status.available
+          : Status.unavailable;
     });
   }
 }
+
+    //   .map((event) {
+    // //print('Connetivity changed2 : $event');
+    // switch (event) {
+    //
+    //   case ConnectivityResult.wifi:
+    //   case ConnectivityResult.ethernet:
+    //   case ConnectivityResult.mobile:
+    //     return Status.available;
+    //
+    //   default:
+    //
+    //     return Status.unavailable;
+    // }
+    //});
+
+
