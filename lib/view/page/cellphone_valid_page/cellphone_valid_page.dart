f15a09c24b6456f4d63b0d42feb57bcb86e81296
Sungdae <sungdae.yi@gmail.com>
2024-05-21 16:23:05 +0200

    faet: 결제 완료 시 장바구니 비움 기능 작성중. 휴대폰 인증기능 구현 준비중

---
+++ lib/view/page/cellphone_valid_page/cellphone_valid_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CellphoneValidPage extends StatelessWidget {
+  const CellphoneValidPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
